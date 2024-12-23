20b95257177fb07b814b229dc523b42c715e7594
Felipe <felipe.a.melo@hotmail.com>
2024-12-23 16:06:56 -0300

    changing db url

--- database/engine.py
+++ database/engine.py
@@ -1,8 +1,8 @@
 from model import page_model, user_model
 from sqlmodel import create_engine, SQLModel
-from settings import DB_URL
+from settings import DB_URL, POSTGRES_URL
 
-engine = create_engine(DB_URL, echo=True)
+engine = create_engine(POSTGRES_URL, echo=True)
 
 
 def get_engine():
